1ef503b8f951bf541f070335b94f5f7ff58345b4
kingjaewon2000 <kkkxx_00@naver.com>
2025-06-17 04:58:51 +0900

    프로젝트 초기화

---
+++ src/main/kotlin/com/example/dadamda/DadamdaApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dadamda
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DadamdaApplication
+
+fun main(args: Array<String>) {
+    runApplication<DadamdaApplication>(*args)
+}
